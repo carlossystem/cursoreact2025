44d80e65fc3a8ad9dbd0e2b7cca8f197d472ea61
carlossystem <carlosjsilvax@gmail.com>
2025-05-19 20:18:35 -0300

    Personalisação

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import logoDidatica from '/logog.svg'
 import './App.css'
 
 function App() {
@@ -10,24 +11,20 @@ function App() {
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={logoDidatica} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="http://globo.com" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Projeto 1 aaa</h1>
+      <h1>Servidor Didatica Digital</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          versão {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+ 
       </div>
-      <p className="read-the-docs">
-        Primeiro projeto react
-      </p>
+
       <p className='read-the-docs'>Desenvolvedor: Carlos José da Silva <br/>Resende RJ</p>
     </>
   )
